3156cae49d4cc8efb48bd964731cb09195510f3d
Sad Gabi <sadgabi20@gmail.com>
2025-11-11 18:29:28 -0600

    refactor/style: tailwind
    - Implemented tailwind.

--- components/ui/UserIcons.tsx
+++ components/ui/UserIcons.tsx
@@ -1,9 +1,15 @@
 interface UserIconProps {
-    imgUrl?: string,
+    imgUrl?: string;
+    className?: string;
+    style?: any;
 }
 
-export default function UserIcon({ imgUrl }: UserIconProps) {
+export default function UserIcon({ imgUrl, className, style }: UserIconProps) {
     return (
-        <div className="user-icon" role="img" style={{backgroundImage: `url(${imgUrl})`}}></div>
-    )
+        <div
+            className={className ?? 'user-icon bg-center bg-cover'}
+            role="img"
+            style={{ backgroundImage: `url(${imgUrl})`, ...(style || {}) }}
+        ></div>
+    );
 }
