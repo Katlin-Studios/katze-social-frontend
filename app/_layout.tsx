62951a0620978f7664583af49caba296da323a71
Sad Gabi <sadgabi20@gmail.com>
2025-11-18 22:24:43 -0600

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,20 @@ import "@/styles/threads.css"
 import Index from "@/app/index"
 import InfoBar from "@/components/ui/InfoBar"
 import SideBar from "@/components/ui/SideBar"
+import MediaViewer from "@/components/ui/threads/MediaViewer"
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { useMediaViewerStore } from "@/hooks/useMediaViewerStore"
 
 const Stack = createNativeStackNavigator()
 
 export default function RootLayout() {
+  const { showMediaViewer } = useMediaViewerStore();
+
   return (<>
     <div className="sides" style={{ justifyContent: "end" }}>
       <SideBar />
     </div>
-    
+
     <Stack.Navigator screenOptions={{ headerShown: false, contentStyle: { backgroundColor: 'transparent' } }}>
       <Stack.Screen name="Home" component={Index} />
     </Stack.Navigator>
@@ -20,5 +24,7 @@ export default function RootLayout() {
     <div className="sides">
       <InfoBar />
     </div>
+
+    {showMediaViewer && <MediaViewer />}
   </>)
 }
